0f9c2bc629af169434a65197afefec7ac053e745
Justin <jstnwrds55@gmail.com>
2020-08-26 10:09:42 -0600

    Added async firestore query functions and tested functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core';
 import './App.css';
 
+import * as Firestore from './components/Firestore'
+
 const styles = {
   
 }
@@ -13,6 +15,47 @@ const styles = {
 function App(props) {
 
   const { classes } = props;
+
+  // // testing addTodo query and async await
+  // Firestore.addtodo("test adding todo").then(() => {
+  //   // testing getAllTodos query
+  //   Firestore.getAllTodos().then(allTodos => {
+  //     allTodos.forEach(doc => {
+  //       var todo = doc.data();
+  //       console.log(todo.body);
+  //     })
+  //   })
+  // })
+
+  // // testing updateTodoBody and async await
+  // Firestore.updateTodoBody("Gql2YEfpeRa8tJ8Orgsw", "testing update new body").then(() => {
+  //   Firestore.getAllTodos().then(allTodos => {
+  //     allTodos.forEach(doc => {
+  //       var todo = doc.data();
+  //       console.log(todo.body);
+  //     })
+  //   })
+  // })
+
+  // // testing updateTodoStatus and async await
+  // Firestore.updateTodoStatus("Gql2YEfpeRa8tJ8Orgsw", "completed").then(() => {
+  //   Firestore.getAllTodos().then(allTodos => {
+  //     allTodos.forEach(doc => {
+  //       var todo = doc.data();
+  //       console.log(todo.status);
+  //     })
+  //   })
+  // })
+
+  // // testing deleteTodo and async await
+  // Firestore.deleteTodo("Gql2YEfpeRa8tJ8Orgsw").then(() => {
+  //   Firestore.getAllTodos().then(allTodos => {
+  //     allTodos.forEach(doc => {
+  //       var todo = doc.data();
+  //       console.log(todo.status);
+  //     })
+  //   })
+  // })
 
   return (
     <div className={classes.container}>
--- src/components/Firestore/index.js
+++ src/components/Firestore/index.js
@@ -18,5 +18,63 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
+// initialize and get ref to firestore
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+// return list of all todos in collection
+export const getAllTodos = async() => {
+    let collection = await db.collection("todos")
+    .get()
+    .catch(error => {
+        console.log("Error getting documents: ", error);
+    });
+    return collection;
+}
+
+// add new todo to db
+export const addtodo = async (body) => {
+    let taskRef = await db.collection("todos").add({
+        body: body,
+        status: "pending"   // automatic set to pending
+    })
+    .catch(error => {
+        console.log("Error adding todo: ", error);
+    })
+    return taskRef;
+}
+
+// update body of a todo
+export const updateTodoBody = async (id, newBody) => {
+    let taskRef = await db.collection("todos").doc(id)
+    .update({
+        body: newBody
+    })
+    .catch(error => {
+        console.log("Error updating body: ", error)
+    })
+    return taskRef;
+}
+
+// update status of a todo
+export const updateTodoStatus = async (id, newStatus) => {
+    let taskRef = await db.collection("todos").doc(id)
+    .update({
+        status: newStatus
+    })
+    .catch(error => {
+        console.log("Error updating status: ", error);
+    })
+    return taskRef;
+}
+
+// delete a todo
+export const deleteTodo = async (id) => {
+    let taskRef = await db.collection("todos")
+    .doc(id)
+    .delete()
+    .catch(error => {
+        console.log("Error deleting todo: ", error);
+    })
+    return taskRef;
+}
