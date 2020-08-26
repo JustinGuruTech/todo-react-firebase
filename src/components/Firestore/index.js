3376eb567922ad0aa6fc966f27fe824ddcdc00d6
Justin <jstnwrds55@gmail.com>
2020-08-26 09:30:31 -0600

    added skeleton for project (firebase/firestore connection, todo components)

---
+++ src/components/Firestore/index.js
@@ -0,0 +1,8 @@
+// Justin Edwards
+// 08/25/2020
+// This file contains the connection to the firestore database
+// on firebase and queries used to perform CRUD operations
+
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
---
+++ src/components/SingleToDo/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+
+}
+
+function SingleToDo(props) {
+    return (
+        <div>
+            Single todo item
+        </div>
+    )
+}
+
+export default withStyles(styles)(SingleToDo);
